813087fe78f6e99fda7aa3a3627687436bfd29a1
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-07-28 18:19:27 -0400

    wrote out delete request, working on post request

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -17,7 +17,6 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
     try {
         const post = await Posts.findById(req.params.id);
-
         if (post) {
             res.status(200).json(post);
         } else {
@@ -40,6 +39,31 @@ router.get("/:id/comments", async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: "The comments information could not be retrieved." });
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const post = await Posts.insert(req.body);
+        if (!req.body.title || !req.body.contents) {
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+        }
+        res.status(201).json(post);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "There was an error while saving the post to the database." });
+    }
+});
+
+router.post("/:id/comments", async (req, res) => {
+    try {
+        const comment = await Posts.insert(req.body);
+        if (!req.body.text) {
+            res.status(400).json({ errorMessage: "Please provide text for the comment." });
+        }
+        res.status(201).json(comment)
+    } catch (error) {
+
     }
 });
 
